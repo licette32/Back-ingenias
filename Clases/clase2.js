77de9aeb60ca26687c75c3540bbc440169ceca19
licette32 <beverly.jlgc@gmail.com>
2025-04-23 00:21:13 -0300

    archivo

--- Clases/clase2.js
+++ Clases/clase2.js
@@ -1,24 +1,48 @@
-// Repasamos conceptos:
-// B. El objeto literal JS
+// Manejo de arrays y Syntactic sugar
 
-// Ejemplo de un objeto literal que representa a una persona
-let persona = {
-    nombre: "Jennyfer",
-    edad: 32,
-    ciudad: "Ushuaia",
-    intereses: ["escritura", "programación", "agricultura"]
+// Ejemplo array llamado hobbies que contiene 4 elementos
+let hobbies = ['escribir', 'cocinar', 'tejer', 'diseñar'];
+
+// acceder al segundo elemento del array
+console.log(hobbies[1]); // cocinar
+
+// Podemos modificar los elementos de array asignando un nuevo valor a su indice correspondiente
+hobbies[3] = 'viajar';
+console.log(hobbies);
+// Salida: [ 'escribir', 'cocinar', 'tejer', 'viajar' ]
+
+
+// Objeto literal JS
+// ejemplo1: obbjeto es el nombre del objeto que est´´as creando, y prop1, prop2, prop3, etc-
+// son propiedades del objeto que esta definido
+
+let objeto = {
+    prop1: 'valor1',
+    prop2: 'valor2',
+    prop3: 'valor3'
 };
 
-// Accediendo a las propiedades del objeto usando la notación de punto
-console.log(persona.nombre); // imprime Jennyfer
-console.log(persona.edad); // imprime 32
+// ejemplo2: persona es el objeto que estamos creando, el cual posee varias propiedades:
+// nombre, edad, dirección, hobbies, estudiante.
 
-// Tambien podemos acceder a las propiedades usando la notación de corchetes
-console.log(persona["ciudad"]); // imprime Ushuaia
-console.log(persona["intereses"][0]); // imprime escritura
+let persona = {
+    nombre: 'Cristofer',
+    edad: 24,
+    direccion: { // objeto anidado dentro de objeto persona
+        calle: 'Av. San Juan',
+        ciudad: 'Brinkmann',
+        pais: 'Argentina'
+    },
+    hobbies: ['Motos', 'correr', 'futbol'], // array de elementos
+    esEstudiante: true,
+    saludar: function() {
+        console.log('Hola, mi nombre es ' + this.nombre);
+    }
+};
 
-// Puedes agregar o odificar propiedades dinámicamente
-persona.profesion = "escritora";
-console.log(persona); // imprime el objeto persona con la nueva propiedad
+// Acceder a las propiedades del objeto persona
+console.log(persona.nombre); // Cristofer
+console.log(persona.direccion.ciudad); // Brinkmann
+console.log(persona.hobbies[1]); // correr
 
-console.log(persona.profesion); // imprime escritora
+// Array de objetos literales
